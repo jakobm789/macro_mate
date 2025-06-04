a9e166c867e721f9d883442e82a5081ca413c853
jakobm789 <112949711+jakobm789@users.noreply.github.com>
2025-06-04 14:20:06 +0200

    Add login page widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:macro_mate/main.dart';
+import 'package:macro_mate/pages/login_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp(initialFilePath: '',));
+  testWidgets('Login page displays title and login button', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: LoginPage(),
+      ),
+    );
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('MacroMate Login'), findsOneWidget);
+    expect(find.text('Login'), findsOneWidget);
   });
 }
